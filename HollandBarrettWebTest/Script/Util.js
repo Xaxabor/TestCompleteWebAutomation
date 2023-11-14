fcb684f12884ce22aa7c3bfd68fa2e6da5adfd72
nadim <nadimmd10@gmail.com>
2023-11-14 22:25:16 +0600

    Revert "- updated"
    This reverts commit ab9773d9b4ac213350375494495b5256d02dc272.

---
+++ HollandBarrettWebTest/Script/Util.js
@@ -0,0 +1,13 @@
+﻿function GenerateRandomString(length) {
+  const characters = 'abcdefghijklmnopqrstuvwxyz';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+}
+
+module.exports ={
+  GenerateRandomString : GenerateRandomString,
+};
